174d29e2062608856f5b5990fa6ce03a32cdc1f5
moonshadow565 <moonshadow565@hotmail.com>
2021-07-22 19:25:32 +0200

    parallel for each

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,23 @@
 #![feature(map_first_last)]
 mod rman;
 use ureq;
+use rayon::prelude::*;
 
 fn main() -> Result<(), String> {
     let dir = "/home/jesus/tmp/lol";
     let url = "/home/jesus/tmp/lol/manifest.manifest";
     let mut agent = ureq::AgentBuilder::new().build();
     let man = rman::Manifest::download(&mut agent, url)?;
-    for file in &man.files {
+    man.files.par_iter().for_each(|file| {
         if !file.langs.contains("none") {
-            continue;
+            return;
         }
         if file.verify(dir) {
             println!("{} is correct!", &file.name);
         } else {
             println!("{} is bad!", &file.name);
         }
-    }
+    });
     Ok(())
 }
 
